5395321f89aa3a46af766bfc12a96cd40e0d5309
Yuki Ishihara <kai@exciter.jp>
2023-07-11 08:52:08 +0900

    簡単なSPAを試作

--- src/resources/ts/app.tsx
+++ src/resources/ts/app.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import { TopPage } from "./pages/TopPage";
+import { MyPage } from "./pages/MyPage";
+import { PostPage } from "./pages/PostPage";
 
 const App = () => {
     return (
-        <h1>Laravel × React</h1>
+        <div id="main">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<TopPage />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/posts" element={<PostPage />} />
+                </Routes>
+                <ul>
+                    <li>
+                        <Link to="/">Top</Link>
+                    </li>
+                    <li>
+                        <Link to="/mypage">My</Link>
+                    </li>
+                    <li>
+                        <Link to="/posts">Post</Link>
+                    </li>
+                </ul>
+            </BrowserRouter>
+        </div>
     );
 };
 
